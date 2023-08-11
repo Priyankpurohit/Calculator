70b108ccfc5b9b6c168498569f7223ca11643a2c
Priyankpurohit <119924112+Priyankpurohit@users.noreply.github.com>
2023-08-11 15:10:15 +0530

    Update script.js
    updated

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-(function () {
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
+//Define function to calculate based on button clicked.
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function (e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-        })
-    });
-
-    equal.addEventListener('click', function (e) {
-        let screen = document.querySelector('.screen');
-        if (screen.value === '') {
-            screen.value = "";
-        } else {
-            let answer = eval(screen.value);
-            screen.value = answer;
-        }
-    })
-
-    clear.addEventListener('click', function (e) {
-        screen.value = "";
-    });
-
-
-
-})();
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
